028281ebb56e34bb755faddef13956bb67c6a6fc
Ben1138 <8263806+Ben1138@users.noreply.github.com>
2021-01-24 15:02:55 +0100

    First working export.

--- LVL2glTF.cpp
+++ LVL2glTF.cpp
@@ -26,7 +26,7 @@ using LibSWBF2::Types::Vector2;
 using LibSWBF2::Types::Vector3;
 using LibSWBF2::Types::Vector4;
 using LibSWBF2::Types::String;
-using LibSWBF2::Types::Color;
+using LibSWBF2::Types::Color4u8;
 using LibSWBF2::Wrappers::World;
 using LibSWBF2::Wrappers::Terrain;
 using LibSWBF2::Wrappers::Instance;
@@ -48,13 +48,142 @@ bool grabLibSWBF2Logs()
     return sthLogged;
 }
 
-void convertColor(const Color& swbfColor, std::vector<double>& outColor)
+void convertColor(const Color4u8& swbfColor, std::vector<double>& outColor)
 {
     outColor.resize(4);
     outColor[0] = swbfColor.m_Red / 255.0;
     outColor[1] = swbfColor.m_Green / 255.0;
     outColor[2] = swbfColor.m_Blue / 255.0;
     outColor[3] = swbfColor.m_Alpha / 255.0;
+}
+
+void copyBuffer(Vector3* srcBuffer, uint32_t srcCount, tinygltf::Buffer& dstBuffer, int dstOffset)
+{
+    for (uint32_t i = 0; i < srcCount; ++i)
+    {
+        int vecIdx = dstOffset + i * sizeof(float) * 3;
+        *reinterpret_cast<float*>(&dstBuffer.data[vecIdx])                     = srcBuffer[i].m_X;
+        *reinterpret_cast<float*>(&dstBuffer.data[vecIdx + sizeof(float)])     = srcBuffer[i].m_Y;
+        *reinterpret_cast<float*>(&dstBuffer.data[vecIdx + sizeof(float) * 2]) = srcBuffer[i].m_Z;
+    }
+}
+
+void copyBuffer(Vector2* srcBuffer, uint32_t srcCount, tinygltf::Buffer& dstBuffer, int dstOffset)
+{
+    for (uint32_t i = 0; i < srcCount; ++i)
+    {
+        int vecIdx = dstOffset + i * sizeof(float) * 2;
+        *reinterpret_cast<float*>(&dstBuffer.data[vecIdx])                 = srcBuffer[i].m_X;
+        *reinterpret_cast<float*>(&dstBuffer.data[vecIdx + sizeof(float)]) = srcBuffer[i].m_Y;
+    }
+}
+
+void copyBuffer(uint16_t* srcBuffer, uint32_t srcCount, tinygltf::Buffer& dstBuffer, int dstOffset)
+{
+    for (uint32_t i = 0; i < srcCount; ++i)
+    {
+        int vecIdx = dstOffset + i * sizeof(uint16_t);
+        *reinterpret_cast<uint16_t*>(&dstBuffer.data[vecIdx]) = srcBuffer[i];
+    }
+}
+
+inline void copyBuffers(
+    Vector3*  swbfVertexBuffer,
+    uint32_t  swbfVertexBufferCount,
+    Vector3*  swbfNormalBuffer,
+    uint32_t  swbfNormalBufferCount,
+    Vector2*  swbfUVBuffer,
+    uint32_t  swbfUVBufferCount,
+    uint16_t* swbfIndexBuffer,
+    uint32_t  swbfIndexBufferCount,
+    tinygltf::Model& dstModel,
+    int& gltfVertexBufferAccIdx,
+    int& gltfNormalBufferAccIdx,
+    int& gltfUVBufferAccIdx,
+    int& gltfIndexBufferAccIdx
+)
+{
+    int segmBufferIdx = 0;
+    int offset = 0;
+
+    uint32_t swbfVertexBufferSize = swbfVertexBufferCount * sizeof(float) * 3;
+    uint32_t swbfNormalBufferSize = swbfNormalBufferCount * sizeof(float) * 3;
+    uint32_t swbfUVBufferSize = swbfUVBufferCount * sizeof(float) * 2;
+    uint32_t swbfIndexBufferSize = swbfIndexBufferCount * sizeof(uint16_t);
+
+    tinygltf::Buffer& segmBuffer = dstModel.buffers.emplace_back();
+    segmBufferIdx = (int)dstModel.buffers.size() - 1;
+    segmBuffer.data.resize(
+        (size_t)swbfVertexBufferSize +
+        (size_t)swbfNormalBufferSize +
+        (size_t)swbfUVBufferSize +
+        (size_t)swbfIndexBufferSize
+    );
+
+    {
+        copyBuffer(swbfVertexBuffer, swbfVertexBufferCount, segmBuffer, offset);
+        tinygltf::BufferView& view = dstModel.bufferViews.emplace_back();
+        view.buffer = segmBufferIdx;
+        view.byteOffset = offset;
+        view.byteLength = swbfVertexBufferSize;
+        view.byteStride = sizeof(float) * 3;
+        offset += swbfVertexBufferSize;
+        tinygltf::Accessor& acc = dstModel.accessors.emplace_back();
+        gltfVertexBufferAccIdx = (int)dstModel.accessors.size() - 1;
+        acc.bufferView = (int)dstModel.bufferViews.size() - 1;
+        acc.byteOffset = 0;
+        acc.componentType = TINYGLTF_COMPONENT_TYPE_FLOAT;
+        acc.type = TINYGLTF_TYPE_VEC3;
+        acc.count = swbfVertexBufferCount;
+    }
+    {
+        copyBuffer(swbfNormalBuffer, swbfNormalBufferCount, segmBuffer, offset);
+        tinygltf::BufferView& view = dstModel.bufferViews.emplace_back();
+        view.buffer = segmBufferIdx;
+        view.byteOffset = offset;
+        view.byteLength = swbfNormalBufferSize;
+        view.byteStride = sizeof(float) * 3;
+        offset += swbfNormalBufferSize;
+        tinygltf::Accessor& acc = dstModel.accessors.emplace_back();
+        gltfNormalBufferAccIdx = (int)dstModel.accessors.size() - 1;
+        acc.bufferView = (int)dstModel.bufferViews.size() - 1;
+        acc.byteOffset = 0;
+        acc.componentType = TINYGLTF_COMPONENT_TYPE_FLOAT;
+        acc.type = TINYGLTF_TYPE_VEC3;
+        acc.count = swbfNormalBufferCount;
+    }
+    {
+        copyBuffer(swbfUVBuffer, swbfUVBufferCount, segmBuffer, offset);
+        tinygltf::BufferView& view = dstModel.bufferViews.emplace_back();
+        view.buffer = segmBufferIdx;
+        view.byteOffset = offset;
+        view.byteLength = swbfUVBufferSize;
+        view.byteStride = sizeof(float) * 2;
+        offset += swbfUVBufferSize;
+        tinygltf::Accessor& acc = dstModel.accessors.emplace_back();
+        gltfUVBufferAccIdx = (int)dstModel.accessors.size() - 1;
+        acc.bufferView = (int)dstModel.bufferViews.size() - 1;
+        acc.byteOffset = 0;
+        acc.componentType = TINYGLTF_COMPONENT_TYPE_FLOAT;
+        acc.type = TINYGLTF_TYPE_VEC2;
+        acc.count = swbfUVBufferCount;
+    }
+    {
+        copyBuffer(swbfIndexBuffer, swbfIndexBufferCount, segmBuffer, offset);
+        tinygltf::BufferView& view = dstModel.bufferViews.emplace_back();
+        view.buffer = segmBufferIdx;
+        view.byteOffset = offset;
+        view.byteLength = swbfIndexBufferSize;
+        view.byteStride = sizeof(uint16_t);
+        offset += swbfIndexBufferSize;
+        tinygltf::Accessor& acc = dstModel.accessors.emplace_back();
+        gltfIndexBufferAccIdx = (int)dstModel.accessors.size() - 1;
+        acc.bufferView = (int)dstModel.bufferViews.size() - 1;
+        acc.byteOffset = 0;
+        acc.componentType = TINYGLTF_COMPONENT_TYPE_UNSIGNED_SHORT;
+        acc.type = TINYGLTF_TYPE_SCALAR;
+        acc.count = swbfIndexBufferCount;
+    }
 }
 
 int gltfTopology(ETopology topology)
@@ -72,7 +201,7 @@ int gltfTopology(ETopology topology)
         case ETopology::TriangleList:
             return TINYGLTF_MODE_TRIANGLES;
         case ETopology::TriangleStrip:
-            return TINYGLTF_MODE_LINE_STRIP;
+            return TINYGLTF_MODE_TRIANGLE_STRIP;
         default:
             LOG("Unknown ETopology type: {0}! Assuming Triangle List!", (int)topology);
             return TINYGLTF_MODE_TRIANGLES;
@@ -83,6 +212,7 @@ int main(int argc, char** argv)
 {
     CLI::App app{ "LVL to glTF converter" };
     std::string fileIn = "";
+    std::string fileCom = "";
     std::string fileOut = "";
     app.add_option("-i,--inlvl", fileIn, "The world LVL file to convert");
     app.add_option("-o,--outglb", fileOut, "(optional) output file");
@@ -113,10 +243,13 @@ int main(int argc, char** argv)
     SWBF2Handle lvlHandle = con->AddLevel(fileIn.c_str());
     con->StartLoading();
 
+    std::vector<char> null = { 0 };
+
     std::string filename = fs::path(fileIn).filename().u8string();
     fmt::memory_buffer updateLine;
-    fmt::format_to(updateLine, "Start Loading '{0}'...\0", filename.c_str());
-    std::cout << updateLine.data() << '\0';
+    fmt::format_to(updateLine, "Start Loading '{0}'...", filename.c_str());
+    updateLine.append(null);
+    std::cout << updateLine.data();
 
     while (!con->IsDone())
     {
@@ -127,37 +260,110 @@ int main(int argc, char** argv)
         else
         {
             updateLine.clear();
-            fmt::format_to(updateLine, "Loading '{0}'... {1}%\0", filename.c_str(), (int)(con->GetOverallProgress() * 100.0f));
-            std::cout << '\r' << updateLine.data() << '\0';
+            fmt::format_to(updateLine, "Loading '{0}'... {1}%", filename.c_str(), (int)(con->GetOverallProgress() * 100.0f));
+            updateLine.append(null);
+            std::cout << '\r' << updateLine.data();
         }
     }
 
     Level* lvl = con->TryGetWorldLevel();
     if (lvl == nullptr)
     {
-        LOG("Loading '{0}' failed!", fileIn.c_str());
+        LOG("Loading '{0}' failed!", filename.c_str());
         return 1;
     }
 
     const List<World> worlds = lvl->GetWorlds();
     if (worlds.Size() == 0)
     {
-        LOG("Seems like '{0}' doesn't contain any world data! Nothing to do...", fileIn.c_str());
+        LOG("Seems like '{0}' doesn't contain any world data! Nothing to do...", filename.c_str());
         return 1;
     }
 
     tinygltf::Model gltf;
+    gltf.asset.copyright = "https://github.com/Ben1138/LVL2glTF";
+    gltf.asset.generator = "LVL2glTF converter";
+    gltf.asset.minVersion = "2.0";
+    gltf.asset.version = "2.0";
+
     std::unordered_map<std::string, int> geomNameToMeshIdx;
 
     for (uint32_t i = 0; i < worlds.Size(); ++i)
     {
         const World& wld = worlds[i];
         String wldName = wld.GetName();
-        //const Terrain* terr = wld.GetTerrain();
+        const Terrain* terr = wld.GetTerrain();
 
         tinygltf::Scene& scene = gltf.scenes.emplace_back();
         scene.name = wldName.Buffer();
         
+        if (terr != nullptr)
+        {
+            tinygltf::Node& terrNode = gltf.nodes.emplace_back();
+            terrNode.name = terr->GetName().Buffer();
+            scene.nodes.emplace_back((int)gltf.nodes.size() - 1);
+            terrNode.translation = { 0.0, 0.0, 0.0 };
+            terrNode.rotation = { 0.0, 0.0, 0.0, 1.0 };
+
+            tinygltf::Mesh& terrMesh = gltf.meshes.emplace_back();
+            int terrMeshIdx = (int)gltf.meshes.size() - 1;
+            terrMesh.name = terr->GetName().Buffer();
+            terrNode.mesh = terrMeshIdx;
+
+            Vector3*  swbfVertexBuffer = nullptr;
+            uint32_t  swbfVertexBufferCount = 0;
+            Vector3*  swbfNormalBuffer = nullptr;
+            uint32_t  swbfNormalBufferCount = 0;
+            Vector2*  swbfUVBuffer = nullptr;
+            uint32_t  swbfUVBufferCount = 0;
+            uint16_t* swbfIndexBuffer = nullptr;
+            uint32_t  swbfIndexBufferCount = 0;
+            int gltfVertexBufferAccIdx = 0;
+            int gltfNormalBufferAccIdx = 0;
+            int gltfUVBufferAccIdx = 0;
+            int gltfIndexBufferAccIdx = 0;
+
+            terr->GetVertexBuffer(swbfVertexBufferCount, swbfVertexBuffer);
+            terr->GetNormalBuffer(swbfNormalBufferCount, swbfNormalBuffer);
+            terr->GetUVBuffer(swbfUVBufferCount, swbfUVBuffer);
+            terr->GetIndexBuffer(ETopology::TriangleList, swbfIndexBufferCount, swbfIndexBuffer);
+
+            copyBuffers(
+                swbfVertexBuffer,
+                swbfVertexBufferCount,
+                swbfNormalBuffer,
+                swbfNormalBufferCount,
+                swbfUVBuffer,
+                swbfUVBufferCount,
+                swbfIndexBuffer,
+                swbfIndexBufferCount,
+                gltf,
+                gltfVertexBufferAccIdx,
+                gltfNormalBufferAccIdx,
+                gltfUVBufferAccIdx,
+                gltfIndexBufferAccIdx
+            );
+
+            // TODO: prevent material duplicates
+            tinygltf::Material& gltfMat = gltf.materials.emplace_back();
+            gltfMat.pbrMetallicRoughness.baseColorFactor = { 1.0, 1.0, 1.0, 1.0 };
+            gltfMat.pbrMetallicRoughness.metallicFactor = 0.0f;
+
+            // TODO: textures
+
+            tinygltf::Primitive& prim = terrMesh.primitives.emplace_back();
+            prim.attributes =
+            {
+                { "POSITION",   gltfVertexBufferAccIdx },
+                { "NORMAL",     gltfNormalBufferAccIdx },
+                { "TEXCOORD_0", gltfUVBufferAccIdx     },
+            };
+
+            prim.indices = gltfIndexBufferAccIdx;
+            prim.mode = TINYGLTF_MODE_TRIANGLES;
+            prim.material = (int)gltf.materials.size() - 1;
+        }
+
         List<Instance> insts = wld.GetInstances();
         for (uint32_t j = 0; j < insts.Size(); ++j)
         {
@@ -167,19 +373,20 @@ int main(int argc, char** argv)
             String geometryName;
             if (!inst.GetProperty("GeometryName", geometryName))
             {
-                LOG("Could not resolve 'GeometryName' property of instance '{0}' in world '{1}'", instName.Buffer(), wldName.Buffer());
+                //LOG("Could not resolve 'GeometryName' property of instance '{0}' in world '{1}'", instName.Buffer(), wldName.Buffer());
                 continue;
             }
             
             const Model* model = con->FindModel(geometryName);
             if (model == nullptr)
             {
-                LOG("Could not find model '{0}' for instance '{1}'!", geometryName.Buffer(), instName.Buffer());
+                //LOG("Could not find model '{0}' for instance '{1}'!", geometryName.Buffer(), instName.Buffer());
                 continue;
             }
 
             tinygltf::Node& node = gltf.nodes.emplace_back();
             node.name = instName.Buffer();
+            scene.nodes.emplace_back((int)gltf.nodes.size() - 1);
 
             Vector3 pos = inst.GetPosition();
             Vector4 rot = inst.GetRotation();
@@ -195,69 +402,77 @@ int main(int argc, char** argv)
             else
             {
                 tinygltf::Mesh& mesh = gltf.meshes.emplace_back();
-                geomNameToMeshIdx.emplace(geometryName.Buffer(), (int)gltf.meshes.size() - 1);
+                int meshIdx = (int)gltf.meshes.size() - 1;
+                geomNameToMeshIdx.emplace(geometryName.Buffer(), meshIdx);
                 mesh.name = model->GetName().Buffer();
+                node.mesh = meshIdx;
+
+                LOG("Converting mesh '{0}'", mesh.name.c_str());
 
                 const List<Segment>& segments = model->GetSegments();
                 for (uint32_t k = 0; k < segments.Size(); ++k)
                 {
                     const Segment& segm = segments[k];
 
-                    tinygltf::Buffer& vertexBuffer = gltf.buffers.emplace_back();
-                    int vertexBufferIdx = (int)gltf.buffers.size() - 1;
+                    Vector3*  swbfVertexBuffer = nullptr;
+                    uint32_t  swbfVertexBufferCount = 0;
+                    Vector3*  swbfNormalBuffer = nullptr;
+                    uint32_t  swbfNormalBufferCount = 0;
+                    Vector2*  swbfUVBuffer = nullptr;
+                    uint32_t  swbfUVBufferCount = 0;
+                    uint16_t* swbfIndexBuffer = nullptr;
+                    uint32_t  swbfIndexBufferCount = 0;
+                    int gltfVertexBufferAccIdx = 0;
+                    int gltfNormalBufferAccIdx = 0;
+                    int gltfUVBufferAccIdx = 0;
+                    int gltfIndexBufferAccIdx = 0;
 
-                    tinygltf::Buffer& normalBuffer = gltf.buffers.emplace_back();
-                    int normalBufferIdx = (int)gltf.buffers.size() - 1;
+                    segm.GetVertexBuffer(swbfVertexBufferCount, swbfVertexBuffer);
+                    segm.GetNormalBuffer(swbfNormalBufferCount, swbfNormalBuffer);
+                    segm.GetUVBuffer(swbfUVBufferCount, swbfUVBuffer);
+                    segm.GetIndexBuffer(swbfIndexBufferCount, swbfIndexBuffer);
 
-                    tinygltf::Buffer& uvBuffer = gltf.buffers.emplace_back();
-                    int uvBufferIdx = (int)gltf.buffers.size() - 1;
+                    copyBuffers(
+                        swbfVertexBuffer,
+                        swbfVertexBufferCount,
+                        swbfNormalBuffer,
+                        swbfNormalBufferCount,
+                        swbfUVBuffer,
+                        swbfUVBufferCount,
+                        swbfIndexBuffer,
+                        swbfIndexBufferCount,
+                        gltf,
+                        gltfVertexBufferAccIdx,
+                        gltfNormalBufferAccIdx,
+                        gltfUVBufferAccIdx,
+                        gltfIndexBufferAccIdx
+                    );
 
-                    tinygltf::Buffer& indexBuffer = gltf.buffers.emplace_back();
-                    int indexBufferIdx = (int)gltf.buffers.size() - 1;
-
-                    uint16_t* idxBuffer = nullptr;
-                    Vector2* vec2Buffer = nullptr;
-                    Vector3* vec3Buffer = nullptr;
-                    uint32_t count = 0;
-
-                    // TODO: copy buffer method, use for loop for copying
-                    segm.GetVertexBuffer(count, vec3Buffer);
-                    vertexBuffer.data.resize(count * sizeof(Vector3));
-                    std::memcpy(vertexBuffer.data.data(), vec3Buffer, count * sizeof(Vector3));
-
-                    segm.GetNormalBuffer(count, vec3Buffer);
-                    normalBuffer.data.resize(count * sizeof(Vector3));
-                    std::memcpy(normalBuffer.data.data(), vec3Buffer, count * sizeof(Vector3));
-
-                    segm.GetUVBuffer(count, vec2Buffer);
-                    uvBuffer.data.resize(count * sizeof(Vector2));
-                    std::memcpy(uvBuffer.data.data(), vec3Buffer, count * sizeof(Vector2));
-                    
-                    segm.GetIndexBuffer(count, idxBuffer);
-                    indexBuffer.data.resize(count * sizeof(uint16_t));
-                    std::memcpy(indexBuffer.data.data(), idxBuffer, count * sizeof(uint16_t));
-
+                    // TODO: prevent material duplicates
                     const Material& swbfMat = segm.GetMaterial();
                     tinygltf::Material& gltfMat = gltf.materials.emplace_back();
                     convertColor(swbfMat.GetDiffuseColor(), gltfMat.pbrMetallicRoughness.baseColorFactor);
                     gltfMat.pbrMetallicRoughness.metallicFactor = 0.0f;
-                    gltfMat.pbrMetallicRoughness.roughnessFactor = 1.0f;
                     
                     // TODO: textures
 
                     tinygltf::Primitive& prim = mesh.primitives.emplace_back();
                     prim.attributes =
                     { 
-                        { "POSITION",   vertexBufferIdx },
-                        { "NORMAL",     normalBufferIdx },
-                        { "TEXCOORD_0", uvBufferIdx     },
+                        { "POSITION",   gltfVertexBufferAccIdx },
+                        { "NORMAL",     gltfNormalBufferAccIdx },
+                        { "TEXCOORD_0", gltfUVBufferAccIdx     },
                     };
-                    prim.indices = indexBufferIdx;
+
+                    prim.indices = gltfIndexBufferAccIdx;
                     prim.mode = gltfTopology(segm.GetTopology());
                     prim.material = (int)gltf.materials.size() - 1;
                 }
+                //break;
             }
+            //break;
         }
+        //break;
     }
 
     con->FreeAll();
@@ -265,9 +480,10 @@ int main(int argc, char** argv)
 
     grabLibSWBF2Logs();
 
+    LOG("Writing output file: {0}...", fileOut.c_str());
     tinygltf::TinyGLTF writer;
     writer.WriteGltfSceneToFile(&gltf, fileOut, false, true, true, true);
-    LOG("Output: {0}", fileOut.c_str());
+    LOG("Done!");
 
     return 0;
 }
